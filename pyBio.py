2d6aeafc9ffac8efb9eef5e94874cd2a5bcc9809
Mphyschem <34543135+flavioolimpio@users.noreply.github.com>
2024-01-14 17:37:36 -0300

    update_catalogo

--- pyBio.py
+++ pyBio.py
@@ -3,45 +3,56 @@ import pandas as pd
 from streamlit_option_menu import option_menu
 st.set_page_config(page_title="pyBio")
 
-def calculate_total(df):
-    df['Preço da venda * Qntd'] = df['Preço da Venda'] * df['Qntd']
-    return df
 
 def main():
-    st.title("Interface de Compra e Revenda de Produtos")
-
     with st.sidebar:
-        option = option_menu('Navegação:', ['Revenda', 'Compra'], 
-        icons=['cart-arrow-down', 'cart-plus'], menu_icon="cast", default_index=0)
+        option = option_menu('Navegação:', ['Catalogo', 'Revenda', 'Compra'], 
+        icons=['list', 'cart-arrow-down', 'cart-plus'], menu_icon="cast", default_index=0)
 
-    if option == 'Revenda':
-        df = pd.read_excel('Tabela_sistema_BioInstitnto.xlsx')
+    df = pd.read_excel('Tabela_sistema_BioInstitnto.xlsx')
+
+    
+    if option == 'Catalogo':
+        st.title("Produtos e Preços de Revenda")
+        st.markdown("Clique aqui para acessar o catálogo de produtos")
+
+        # Calcula o preço de revenda para cada produto
+        df['Preço de Revenda'] = df.iloc[:, 1] + df.iloc[:, 1] * 0.51
+
+        # Seleciona apenas as colunas 'Produto' e 'Preço de Revenda'
+        products_df = df[['Produto', 'Preço de Revenda']]
+
+        # Exibe a tabela de produtos e preços de revenda
+        st.dataframe(products_df)
+    
+    elif option == 'Revenda':
+        st.title("Interface de Revenda de Produtos")
         df['Qntd'] = [0]*len(df)
-        df['Preço da Venda'] = [0.0]*len(df)
-        df['Preço da venda * Qntd'] = [0.0]*len(df)
+        df['Preço Final'] = [0.0]*len(df)
 
         for i in range(len(df)):
             st.subheader(f'Produto: {df.iloc[i, 0]}')
             quantity = st.number_input(f'Insira a quantidade do produto {df.iloc[i, 0]}', min_value=0, value=0, key=str(i))
-            price = df.iloc[i, 2] * 0.5 * quantity  # Calcula o preço de revenda
+            price = (df.iloc[i, 1] + df.iloc[i, 1] * 0.51) * quantity  # Calcula o preço de revenda
             df.loc[i, 'Qntd'] = quantity
-            df.loc[i, 'Preço da Venda'] = price
-            df.loc[i, 'Preço da venda * Qntd'] = quantity * price
+            df.loc[i, 'Preço Final'] = price
 
             # Exibe o valor de revenda calculado
             st.text(f'Preço de revenda do produto {df.iloc[i, 0]}: R$ {price:.2f}')
 
         if st.button('Processar Planilha de Revenda'):
-            df = df[df['Preço da venda * Qntd'] > 0]  # Exclui produtos com valor zero
-            result = calculate_total(df)
-            st.write(result)
-            total_value = result['Preço da venda * Qntd'].sum()
+            df = df[df['Preço Final'] > 0]  # Exclui produtos com valor zero
+            total_value = df['Preço Final'].sum()
             st.write('O valor total a ser repassado para o cliente é: ', total_value)
 
-            result.to_excel('nova_planilha_revenda.xlsx', index=False)
+            # Seleciona apenas as colunas 'Qntd' e 'Preço Final'
+            final_df = df[['Produto', 'Qntd', 'Preço Final']]
+
+            # Exibe a tabela final
+            st.dataframe(final_df)
+
+            df.to_excel('nova_planilha_revenda.xlsx', index=False)
             st.success('Nova planilha de Excel de revenda criada com sucesso!')
-
-
 
     elif option == 'Compra':
         df = pd.read_excel('Tabela_sistema_BioInstitnto.xlsx')
